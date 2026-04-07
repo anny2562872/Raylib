7e4d167c79a55b01872c13d4753bf69211df3e39
anny2562872 <andrewjlebovich@gmail.com>
2026-04-07 01:11:08 -0400

    First Commit

---
+++ makefile
@@ -0,0 +1,14 @@
+# Define variables for the compiler and flags
+CXX = gcc
+CXXFLAGS = -lraylib -lX11 -lGL -lm -ldl -lrt 
+
+# The first target is the default one run by 'make'
+all: main
+
+# Rule to create the 'main' executable
+main: raylibFile.c
+	$(CXX) -o ./bin/raylibFile raylibFile.c $(CXXFLAGS) 
+# A 'clean' rule to remove the executable
+clean:
+	rm -f ./bin/raylibFile
+
---
+++ raylibFile.c
@@ -0,0 +1,64 @@
+#include <raylib.h> 
+#include <stdio.h> 
+
+int main(void){
+
+typedef struct {
+	Vector2 postion;
+	float speed;
+} actor;
+
+const float Height = GetMonitorHeight(0);
+const float Width = GetMonitorWidth(0);
+int TextSize = 50;
+float VertVel = 0;
+float HoriVel = 0;
+actor Player = {{0,0}, 60};
+float Damping = 0.9;
+Camera2D camera = {0};
+camera.zoom = 1.0f;
+bool showCords = 1;
+InitWindow(Width, Height, "Test window");
+SetTargetFPS(60);
+while (!WindowShouldClose()){
+	// TO-DO:
+	// - make the camera drag behind the player
+	camera.target = (Vector2){Player.postion.x,Player.postion.y};
+	camera.offset = (Vector2){GetMonitorWidth(0)/2.0f - 25.0f,GetMonitorHeight(0)/2.0f - 25.0f};
+	// Checking for player input
+	if (IsKeyDown(KEY_W)){VertVel -= 1;}
+	if (IsKeyDown(KEY_S)){VertVel += 1;}
+	if (IsKeyDown(KEY_D)){HoriVel += 1;}
+	if (IsKeyDown(KEY_A)){HoriVel -= 1;}
+	// Apply velocity to player postion
+	Player.postion.x += GetFrameTime()*HoriVel*Player.speed; //Move player in the Vertical direction with deltatime
+	Player.postion.y += GetFrameTime()*VertVel*Player.speed; //Move player in the Horizontal direction with deltatime
+	// Damping the velocities
+	VertVel *= Damping;
+	HoriVel *= Damping;
+	//Show cords when tab is pressed
+	char StrPosX[128];
+	char StrPosY[128];
+	if (IsKeyPressed(KEY_TAB)){
+		if (showCords){ showCords = false; }
+		else {showCords = true;}
+	}
+	snprintf(StrPosX,sizeof(StrPosX),"x: %.2f",Player.postion.x);
+	snprintf(StrPosY,sizeof(StrPosY),"y: %.2f",Player.postion.y);
+	// Drawing to the screen
+	BeginDrawing();
+		ClearBackground(RAYWHITE);
+		BeginMode2D(camera);
+			DrawRectangle(0,0,100,100,GRAY);
+			DrawRectangle(Player.postion.x,Player.postion.y,50,50,RED);
+		EndMode2D();
+
+		if (showCords){
+			DrawText(StrPosX,10,10,50,BLACK);
+			DrawText(StrPosY,10,60,50,BLACK);
+		}
+	EndDrawing();
+} // Runtime
+
+
+} // Main
